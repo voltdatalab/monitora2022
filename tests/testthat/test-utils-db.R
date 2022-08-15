8c9c36c3194a09d2229451b0e9a5c9515159b337
rmhirota <re.hirota@gmail.com>
2022-08-15 13:29:38 -0300

    rm test db

--- tests/testthat/test-utils-db.R
+++ tests/testthat/test-utils-db.R
@@ -1,4 +1,3 @@
-test_that("db has password", {
-  senha_db <- Sys.getenv("DB_PASSWORD")
-  expect_gt(stringr::str_length(senha_db), 0)
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
 })
